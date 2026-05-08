56cf62f1610490f1aefd373ade7dbcfa2b8b807a
noxcenary <har1001sha@gmail.com>
2026-05-08 20:10:55 +0530

    debugged

--- binary_search_demo.py
+++ binary_search_demo.py
@@ -3,14 +3,14 @@ import binary_search
 
 input_numbers=[]
 
-for i in range range(1,len(sys.argv)):
+for i in  range(1,len(sys.argv)):
     input_numbers.append(float(sys.argv[i]))
 
 print(f"User given elements are \n",input_numbers)
 
 search_element=float(input("enter the element to be searched "))
 
-search_index=binary_(search_element,input_numbers)
+search_index=binary_search(search_element,input_numbers)
 
 if search_index==-1:
     print("element not found")
